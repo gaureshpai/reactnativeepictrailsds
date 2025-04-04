7150d93e44c482ab5f5636a615f1731b02053220
Milan-C-I <milancheriyamanep@gmail.com>
2025-04-04 13:04:01 +0530

    grid done

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -1,9 +1,52 @@
 import { Text, View } from "react-native";
-import { Tile } from "../components/Tile";
+import { Grid } from "../components/grid";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-     <Tile w={300} h={300}  label="label"  paragraph="Paragraph ajknvjkv jkavnjka vkjanvjknv avjknavnkavjvn jkavnjvnjv nvkavnjnv" icon="box"  variant="variant-2" enabled={true} selected={false}/>
+      <View className="space-x-10">
+      <Grid container spacing={2} rows={2}>
+    <Grid.Item size={4}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=8</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={1}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=4</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={1}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=4</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={1}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=8</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={1}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=8</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={1}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=4</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={1}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=4</Text>
+      </View>
+    </Grid.Item>
+    <Grid.Item size={8}>
+      <View style={{ height: 50, backgroundColor: '#333' }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>xs=6 md=8</Text>
+      </View>
+    </Grid.Item>
+  </Grid>
+      </View>
     </View>
   );
 }
