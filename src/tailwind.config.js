c260a45c9db2a5a688f8156d05fe27c584b8eb4f
HimanshuHegde <himanshuhegde797@gmail.com>
2025-04-09 10:02:37 +0530

    made some more changes

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}","./dist/**/*.{js,jsx,ts,tsx}"],
+  content: ["./**/*.{js,jsx,ts,tsx}","../dist/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
