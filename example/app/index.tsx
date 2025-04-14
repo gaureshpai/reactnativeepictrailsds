8822a7d8c102d0b3e946f7729b6adc2eb27b9650
HimanshuHegde <himanshuhegde797@gmail.com>
2025-04-14 13:28:05 +0530

    removal of /example

--- example/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-import { RectButton } from "../components/Button";
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <RectButton label="df" onPress={() => alert("pressed")}/>
-    </View>
-  );
-}
