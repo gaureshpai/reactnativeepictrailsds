aedc8b9f7fc5f97541e2a211992decdcc85481ed
Milan-C-I <milancheriyamanep@gmail.com>
2025-04-18 12:30:33 +0530

    navigation bar ios/android

--- allprops.ts
+++ allprops.ts
@@ -255,3 +255,12 @@ export interface StoryProgressBarsProps {
   activeStoryIndex: number;
 }
 
+export interface NavigationBarProps {
+  title: string;
+  device?: 'ios' | 'android';
+  action?: 1 | 2 | 3;
+  size?: 'compact' | 'expandable';
+  onClose?: () => void;
+  onClipboard?: () => void;
+  onForward?: () => void;
+}
